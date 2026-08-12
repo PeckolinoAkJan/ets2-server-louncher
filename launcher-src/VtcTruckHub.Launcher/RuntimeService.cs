a4668cd37bcdc38c6b6f1f4acf775b62361e552c
PeckolinoAkJan <j.rikeit@gmail.com>
2026-08-12 22:11:35 +0200

    Version synchronized launcher as 0.9.1

--- launcher-src/VtcTruckHub.Launcher/RuntimeService.cs
+++ launcher-src/VtcTruckHub.Launcher/RuntimeService.cs
@@ -6,7 +6,7 @@ namespace VtcTruckHub.Launcher;
 
 public sealed class RuntimeService : IDisposable
 {
-    private const string ExpectedRuntimeVersion = "0.9.0";
+    private const string ExpectedRuntimeVersion = "0.9.1";
     private readonly string root = AppContext.BaseDirectory;
     private Process? serviceProcess;
 
